9ea22cab69b5c6e0a8de8173cef2a17c8fc77f08
Seiya Miyata <odradek38@gmail.com>
2023-06-08 22:12:23 +0900

    Run bundle gem mergration

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+## [Unreleased]
+
+## [0.1.0] - 2023-06-08
+
+- Initial release
---
+++ lib/mergration.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mergration/version"
+
+module Mergration
+  class Error < StandardError; end
+  # Your code goes here...
+end

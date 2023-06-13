66677818c70ed6ada90b1fcc4ce35c348800c22f
Seiya Miyata <odradek38@gmail.com>
2023-06-14 03:12:42 +0900

    Add debug printing

--- lib/mergration/parser.rb
+++ lib/mergration/parser.rb
@@ -13,7 +13,7 @@ module Mergration
       text = File.read(path)
 
       ast = Kramdown::Document.new(text, input: DOCUMENT_INPUT).to_hash_ast
-      puts ast[:children].select { |k, _| k[:type] == :entity }.map { |e| e[:options] }
+      puts ast
       ast[:children].select { |k, _| k[:type] == :entity }.map { |e| e[:options] }
     end
   end

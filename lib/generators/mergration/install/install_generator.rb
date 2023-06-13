5cb4b5fcf0798abe78575314e8435dd60d536f61
Seiya Miyata <odradek38@gmail.com>
2023-06-14 00:07:20 +0900

    Implement Mergration::InstallGenerator#parse_file

--- lib/generators/mergration/install/install_generator.rb
+++ lib/generators/mergration/install/install_generator.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative '../migration_generator'
+require_relative '../../../mergration/parser'
 
 module Mergration
   class InstallGenerator < MigrationGenerator
@@ -9,9 +10,20 @@ module Mergration
     desc 'Generates a migration from entities described on mermaid.'
 
     def create_migration_file
+      parse_file
       add_mergration_migration(
         'create_dummy_tables'
       )
+    end
+
+    def parse_file
+      files = Dir.glob(File.expand_path('docs/mermaid/*.md'))
+
+      results = []
+      files.each do |file|
+        results << Mergration::Parser.parse(file)
+      end
+      results
     end
   end
 end
